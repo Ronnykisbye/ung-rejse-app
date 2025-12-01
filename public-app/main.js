1d864f04eb2881f30e747da2ae8a7b7fdfdef967
Ronny kisbye <111735396+Ronnykisbye@users.noreply.github.com>
2025-12-01 13:45:17 +0100

    Create main.js

---
+++ public-app/main.js
@@ -0,0 +1,14 @@
+// --------------------------------------------------
+// UNG REJS – App Start
+// ReactDOM mount i GitHub Pages
+// --------------------------------------------------
+
+const rootElement = document.getElementById("root");
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
